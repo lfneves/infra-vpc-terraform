8f2938ad5aafea2a05a68e65cc8e04c2c4b8d469
lfneves <lfneves.dg@gmail.com>
2023-10-20 00:42:38 -0300

    fix: update vpc repository

--- infra-vpc-terraform/vpc.tf
+++ infra-vpc-terraform/vpc.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -21,7 +21,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   count           = 2
   vpc_id          = aws_vpc.my_vpc.id
-  cidr_block      = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)  # Use /24 for non-overlapping subnets
+  cidr_block      = "10.0.1.0/24"
   availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
 }
 
