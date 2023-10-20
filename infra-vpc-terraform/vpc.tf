e8abecb7918d6633ee4c776a5d6d8bddf9d0d941
lfneves <lfneves.dg@gmail.com>
2023-10-20 01:00:15 -0300

    fix: update vpc repository

--- infra-vpc-terraform/vpc.tf
+++ infra-vpc-terraform/vpc.tf
@@ -35,7 +35,7 @@ resource "aws_route_table" "my_route_table" {
 
 resource "aws_route" "route_to_internet" {
   route_table_id = aws_route_table.my_route_table.id
-  destination_cidr_block = "10.0.0.0/28"
+  destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.my_igw.id
 }
 
